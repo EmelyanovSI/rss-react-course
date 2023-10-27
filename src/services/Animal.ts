b2b012922952ab6c139a6b4b06e09a4d8c7ac11a
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-10-28 02:45:59 +0300

    feat(Animal.ts): add fetchPage function to fetch animal data from API based on search value

---
+++ src/services/Animal.ts
@@ -0,0 +1,28 @@
+import { VITE_BASE_URL } from '../constants';
+import { AnimalResponse } from '../interfaces/Animal';
+
+export async function fetchPage(searchValue: string): Promise<AnimalResponse> {
+  const apiUrl = `${VITE_BASE_URL}/animal/search`;
+  const requestBody = new URLSearchParams();
+  requestBody.append('name', searchValue);
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: requestBody.toString(),
+  };
+
+  try {
+    const response = await fetch(apiUrl, requestOptions);
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error('Failed to fetch data');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    return {} as AnimalResponse;
+  }
+}
