662b7cb2df59016be919cc59477d2f3707edb399
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-10-29 21:06:11 +0300

    feat(Button.tsx): add Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,22 @@
+import { Component, ReactNode } from 'react';
+
+interface ButtonProps {
+  children: ReactNode;
+  onClick: () => void;
+}
+
+class Button extends Component<ButtonProps> {
+  constructor(props: ButtonProps) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <button type="button" className="button" onClick={this.props.onClick}>
+        {this.props.children}
+      </button>
+    );
+  }
+}
+
+export default Button;
