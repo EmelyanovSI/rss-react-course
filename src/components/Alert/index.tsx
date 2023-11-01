da007674bc7449575b0a35403d6f545c74bbc059
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-11-02 00:29:54 +0300

    refactor(enums.ts): add Severity enum

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -1,12 +1,6 @@
 import classNames from 'classnames';
 import { FC } from 'react';
-
-enum Severity {
-  success = 'success',
-  error = 'error',
-  warning = 'warning',
-  info = 'info',
-}
+import { Severity } from '../../constants/enums';
 
 interface AlertProps {
   message: string;
@@ -14,23 +8,24 @@ interface AlertProps {
 }
 
 const Alert: FC<AlertProps> = ({ message, severity }) => {
+  const { success, error, warning, info } = Severity;
   return (
     <div
       className={classNames(
         'flex items-center gap-2',
         'border rounded-full h-10 px-6',
         'transition duration-500 ease-in-out hover:shadow',
-        { 'bg-green-100 text-green-700': severity === Severity.success },
-        { 'bg-red-100 text-red-700': severity === Severity.error },
-        { 'bg-yellow-100 text-yellow-700': severity === Severity.warning },
-        { 'bg-blue-100 text-blue-700': severity === Severity.info }
+        { 'bg-green-100 text-green-700': severity === success },
+        { 'bg-red-100 text-red-700': severity === error },
+        { 'bg-yellow-100 text-yellow-700': severity === warning },
+        { 'bg-blue-100 text-blue-700': severity === info }
       )}
     >
       <span className="material-symbols-outlined select-none">
-        {severity === Severity.success && 'check_circle'}
-        {severity === Severity.error && 'error_outline'}
-        {severity === Severity.warning && 'warning'}
-        {severity === Severity.info && 'info'}
+        {severity === success && 'check_circle'}
+        {severity === error && 'error_outline'}
+        {severity === warning && 'warning'}
+        {severity === info && 'info'}
       </span>
       <span>{message}</span>
     </div>
--- src/constants/enums.ts
+++ src/constants/enums.ts
@@ -4,3 +4,10 @@ export enum Status {
   Succeeded,
   Failed,
 }
+
+export enum Severity {
+  success = 'success',
+  error = 'error',
+  warning = 'warning',
+  info = 'info',
+}
