b8ff65ffaebf271c71578e768bbdad85c950a4cb
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-11-05 19:38:11 +0300

    fix(ErrorPage.tsx): remove unnecessary check for isRouteErrorResponse and update error message display

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,31 +1,24 @@
-import { isRouteErrorResponse, Link, useRouteError } from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
 import Alert from '../components/Alert';
 import Button from '../components/Button';
 
 const ErrorPage = () => {
-  const error = useRouteError();
+  const error = useRouteError() as Error;
   console.error(error);
 
-  if (isRouteErrorResponse(error)) {
-    return (
-      <div className="flex flex-col items-center gap-6 p-6">
-        <Alert severity="warning" message="Oops!" />
-        <Alert
-          severity="error"
-          message="Sorry, an unexpected error has occurred."
-        />
-        <Alert
-          severity="info"
-          message={error.statusText || error.data.message}
-        />
-        <Link to="/">
-          <Button icon="home">Home page</Button>
-        </Link>
-      </div>
-    );
-  }
-
-  return null;
+  return (
+    <div className="flex flex-col items-center gap-6 p-6">
+      <Alert severity="warning" message="Oops!" />
+      <Alert
+        severity="error"
+        message="Sorry, an unexpected error has occurred."
+      />
+      <Alert severity="info" message={error && error.message} />
+      <Link to="/">
+        <Button icon="home">Home page</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default ErrorPage;
