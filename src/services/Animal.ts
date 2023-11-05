7e2bb19c7212df8eb38645b23ddbd1ef62c09d71
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-11-05 04:40:51 +0300

    feat(Animal.ts): add fetchAnimal function to fetch data for a specific animal by uid

--- src/services/Animal.ts
+++ src/services/Animal.ts
@@ -1,11 +1,11 @@
 import { VITE_BASE_URL } from '../constants';
-import { AnimalResponse } from '../interfaces/Animal';
+import { AnimalPageResponse, AnimalResponse } from '../interfaces/Animal';
 
 export async function fetchPage(
-  searchValue: string,
+  searchValue: string = '',
   pageNumber = 0,
   pageSize = 100
-): Promise<AnimalResponse> {
+): Promise<AnimalPageResponse> {
   const apiUrl = `${VITE_BASE_URL}/animal/search?pageNumber=${pageNumber}&pageSize=${pageSize}`;
   const requestBody = new URLSearchParams();
   requestBody.append('name', searchValue);
@@ -16,6 +16,29 @@ export async function fetchPage(
       'Content-Type': 'application/x-www-form-urlencoded',
     },
     body: requestBody.toString(),
+  };
+
+  try {
+    const response = await fetch(apiUrl, requestOptions);
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error('Failed to fetch data');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    return {} as AnimalPageResponse;
+  }
+}
+
+export async function fetchAnimal(uid: string): Promise<AnimalResponse> {
+  const apiUrl = `${VITE_BASE_URL}/animal?uid=${uid}`;
+
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+    },
   };
 
   try {
