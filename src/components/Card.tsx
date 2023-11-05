d1b8f1c89e71ccbebd75f46a72bd371e31d6ffa5
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-11-05 04:28:47 +0300

    feat(Card.tsx): add onClick prop to Card component to enable click functionality

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,17 +4,19 @@ import { FC, ReactNode } from 'react';
 interface CardProps {
   children: ReactNode;
   title: string;
+  onClick?: () => void;
 }
 
-const Card: FC<CardProps> = ({ title, children }) => {
+const Card: FC<CardProps> = ({ title, children, onClick }) => {
   return (
     <div
       className={classNames(
-        'flex flex-col place-content-between',
+        'flex flex-col place-content-between border rounded-lg',
         'transition duration-500 ease-in-out',
-        'border rounded-lg hover:shadow-md hover:text-red-500',
-        'bg-red-50 w-64 p-2 gap-6'
+        'active:shadow-none hover:shadow-md hover:text-red-500',
+        'bg-red-50 w-64 p-2 gap-6 cursor-pointer'
       )}
+      onClick={onClick}
     >
       <h2 className="font-medium text-black">{title}</h2>
       {children}
