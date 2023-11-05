c655e2f327849b1c85b5b758ff1c66974a4a659a
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-11-05 03:54:01 +0300

    feat(Content.tsx): add Nav and Pagination components to the Content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,6 +2,8 @@ import { FC, ReactNode } from 'react';
 import { Status } from '../constants/enums';
 import { Loading } from '../interfaces/Loading';
 import Alert from './Alert';
+import Nav from './Nav';
+import Pagination from './Pagination';
 import Progress from './Progress';
 
 interface ContentProps extends Loading {
@@ -9,20 +11,27 @@ interface ContentProps extends Loading {
 }
 
 const Content: FC<ContentProps> = ({ status, message, children }) => {
-  switch (status) {
-    case Status.Idle:
-      return null;
-    case Status.Loading:
-      return <Progress />;
-    case Status.Failed:
-      return (
-        <div className="flex justify-center p-6">
-          <Alert message={message} severity="error" />
-        </div>
-      );
-    default:
-      return <div className="flex justify-center p-6">{children}</div>;
-  }
+  const renderContent = () => {
+    switch (status) {
+      case Status.Idle:
+        return null;
+      case Status.Loading:
+        return <Progress />;
+      case Status.Failed:
+        return <Alert message={message} severity="error" />;
+      default:
+        return children;
+    }
+  };
+
+  return (
+    <>
+      <Nav>
+        <Pagination />
+      </Nav>
+      <div className="flex justify-center p-6">{renderContent()}</div>
+    </>
+  );
 };
 
 export default Content;
