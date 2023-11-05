57f237304365f58f32e24a18d3e994fe9a0eefb0
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-11-05 04:47:58 +0300

    refactor(main.tsx): reorganize code structure and improve readability

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,62 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Navigate,
+  Params,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import './index.css';
+import { AnimalPageResponse, AnimalResponse } from './interfaces/Animal';
+import CardLayout from './pages/CardLayout';
+import CardPage from './pages/CardPage';
 import ErrorPage from './pages/ErrorPage';
-import Root from './pages/Root';
+import ListLayout from './pages/ListLayout';
+import List from './pages/ListPage';
+import RootLayout from './pages/RootLayout';
+import { fetchAnimal, fetchPage } from './services/Animal';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-  },
-]);
+const listLoader = async ({
+  params,
+}: {
+  params: Params;
+}): Promise<AnimalPageResponse> => {
+  return await fetchPage(params.search, +params.number! - 1);
+};
+
+const cardLoader = async ({
+  params,
+}: {
+  params: Params;
+}): Promise<AnimalResponse> => {
+  return await fetchAnimal(params.uid!);
+};
+
+const renderRoutes = () => (
+  <>
+    <Route index element={<Navigate to="1" replace />} />
+    <Route path=":number" element={<List />} loader={listLoader}>
+      <Route path=":uid" element={<CardLayout />}>
+        <Route index element={<CardPage />} loader={cardLoader} />
+      </Route>
+    </Route>
+  </>
+);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
+      <Route path="page" element={<ListLayout />}>
+        {renderRoutes()}
+      </Route>
+      <Route path=":search/page" element={<ListLayout />}>
+        {renderRoutes()}
+      </Route>
+    </Route>
+  )
+);
 
 const rootElement = document.getElementById('root');
 
