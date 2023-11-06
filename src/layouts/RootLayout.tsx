9363bbabc2db48b5c0a2d63eb0847e12a8c12dc9
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-11-07 01:27:45 +0300

    feat(RootLayout.tsx): add support for search query parameter in URL to enable searching functionality

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,44 +1,57 @@
-import { FC, useEffect } from 'react';
-import { generatePath, Outlet, useNavigate, useParams } from 'react-router-dom';
+import { FC } from 'react';
+import {
+  Outlet,
+  useNavigate,
+  useParams,
+  useSearchParams,
+} from 'react-router-dom';
 import Search from '@/components/common/Search';
-import ErrorBoundary from '@/components/ErrorBoundary';
 import ErrorBoundaryButton from '@/components/ErrorBoundaryButton';
 import Header from '@/components/Header';
+import { RouterParams } from '@/constants';
+import {
+  getOriginalPath,
+  getSearchFromStorage,
+  setSearchFromStorage,
+} from '@/utils';
 
-interface RootLayoutProps {
-  searchValue: string;
-  setSearchValue: (searchValue?: string) => void;
-}
-
-const RootLayout: FC<RootLayoutProps> = ({ searchValue, setSearchValue }) => {
-  const { search, uid = '' } = useParams();
+const RootLayout: FC = () => {
+  const { limit = '10', details = '' } = useParams<RouterParams>();
+  const [searchParams] = useSearchParams();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    setSearchValue(search);
-  }, [search, setSearchValue]);
-
-  const navigateTo = (search: string) => {
-    const to = generatePath(':search/page/:number/:uid', {
-      search,
-      number: '1',
-      uid,
-    });
-    navigate(to);
+  const getSearchParam = () => {
+    return searchParams.get('search') ?? '';
   };
 
-  const handleSearch = (searchValue: string) => {
-    navigateTo(searchValue);
+  const setSearchParam = (search?: string) => {
+    if (search) {
+      searchParams.set('search', search);
+    } else {
+      searchParams.delete('search');
+    }
   };
+
+  const handleSearch = (search: string) => {
+    setSearchParam(search);
+    setSearchFromStorage(search);
+    const pathname = getOriginalPath(limit, details);
+    navigate({ pathname, search: `${searchParams}` });
+  };
+
+  const search = getSearchParam();
+  if (search) {
+    setSearchFromStorage(search);
+  }
 
   return (
-    <ErrorBoundary>
+    <>
       <Header>
         <ErrorBoundaryButton />
-        <Search value={search ?? searchValue} onSearch={handleSearch} />
+        <Search value={getSearchFromStorage()} onSearch={handleSearch} />
       </Header>
       <Outlet />
-    </ErrorBoundary>
+    </>
   );
 };
 
