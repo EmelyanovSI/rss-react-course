2eee24e472e7c61b77084a608a3430f06a851cdd
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-11-07 01:19:06 +0300

    feat(App.tsx): add fallback loading component to display a progress indicator while the app is loading

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,12 @@
 import { RouterProvider } from 'react-router-dom';
+import Progress from '@/components/common/Progress';
 import ErrorBoundary from '@/components/ErrorBoundary';
 import router from '@/router';
 
 const App = () => {
   return (
     <ErrorBoundary>
-      <RouterProvider router={router} />
+      <RouterProvider router={router} fallbackElement={<Progress />} />
     </ErrorBoundary>
   );
 };
