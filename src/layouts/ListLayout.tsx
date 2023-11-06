1bbf211fdb4ba83a6d4e274c1757bf0dcdc74561
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-11-07 01:25:21 +0300

    feat(ListLayout.tsx): add support for search parameters in navigation

--- src/layouts/ListLayout.tsx
+++ src/layouts/ListLayout.tsx
@@ -1,36 +1,36 @@
 import { FC, useState } from 'react';
 import {
-  generatePath,
   Outlet,
   useNavigate,
   useNavigation,
   useParams,
+  useSearchParams,
 } from 'react-router-dom';
 import Pagination from '@/components/common/Pagination';
 import Progress from '@/components/common/Progress';
 import Nav from '@/components/Nav';
+import { RouterParams } from '@/constants';
+import { getOriginalPath } from '@/utils';
 
 const ListLayout: FC = () => {
-  const { number = 1, uid = '' } = useParams();
+  const { page = '1', limit = '10', details = '' } = useParams<RouterParams>();
+  const [searchParams] = useSearchParams();
   const [first, setFirst] = useState(true);
   const [last, setLast] = useState(true);
   const navigate = useNavigate();
   const { state } = useNavigation();
 
-  const navigateTo = (number: string) => {
-    const to = generatePath(':number/:uid', {
-      number,
-      uid,
-    });
-    navigate(to);
+  const navigateTo = (newPage: string) => {
+    const pathname = getOriginalPath(limit, details, newPage);
+    navigate({ pathname, search: `${searchParams}` });
   };
 
   const handlePrevClick = () => {
-    navigateTo(`${+number - 1}`);
+    navigateTo(`${+page - 1}`);
   };
 
   const handleNextClick = () => {
-    navigateTo(`${+number + 1}`);
+    navigateTo(`${+page + 1}`);
   };
 
   if (state === 'loading') {
