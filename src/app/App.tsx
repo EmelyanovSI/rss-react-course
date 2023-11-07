2ae579d1ea30e4704fff188c13486b626ff76563
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-11-07 19:37:54 +0300

    feat(App.tsx): add AppProvider to wrap the entire app to provide context to all components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,15 @@
 import Progress from '@/components/common/Progress';
 import ErrorBoundary from '@/components/ErrorBoundary';
+import AppProvider from '@/context';
 import router from '@/router';
 import { RouterProvider } from 'react-router-dom';
 
 const App = () => {
   return (
     <ErrorBoundary>
-      <RouterProvider router={router} fallbackElement={<Progress />} />
+      <AppProvider>
+        <RouterProvider router={router} fallbackElement={<Progress />} />
+      </AppProvider>
     </ErrorBoundary>
   );
 };
