b499a46d216578f4c198c495967e535fe319c9f9
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-11-08 21:46:31 +0300

    feat(router.tsx): remove unnecessary loader functions as they are not being used in the code

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,3 @@
-import { RouterParams } from '@/constants';
-import { AnimalPageResponse, AnimalResponse } from '@/interfaces/animal';
 import CardLayout from '@/layouts/CardLayout';
 import ListLayout from '@/layouts/ListLayout';
 import RootLayout from '@/layouts/RootLayout';
@@ -7,47 +5,12 @@ import CardPage from '@/pages/CardPage';
 import ErrorPage from '@/pages/ErrorPage';
 import ListPage from '@/pages/ListPage';
 import NotFoundPage from '@/pages/NotFoundPage';
-import { fetchAnimal, fetchPage } from '@/services/animal';
-import { getSearchFromStorage } from '@/utils';
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Navigate,
-  Params,
   Route,
 } from 'react-router-dom';
-
-const listLoader = async ({
-  params,
-  request,
-}: {
-  params: Params<RouterParams>;
-  request: Request;
-}): Promise<AnimalPageResponse> => {
-  const search =
-    new URL(request.url).searchParams.get('search') ?? getSearchFromStorage();
-  const result = await fetchPage(search, Number(params.page) - 1, params.limit);
-
-  if (!result.ok) {
-    throw Error('Could not find animals');
-  }
-
-  return result.json();
-};
-
-const cardLoader = async ({
-  params,
-}: {
-  params: Params<RouterParams>;
-}): Promise<AnimalResponse> => {
-  const result = await fetchAnimal(params.details);
-
-  if (!result.ok) {
-    throw Error('Could not find more info');
-  }
-
-  return result.json();
-};
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -61,15 +24,9 @@ const router = createBrowserRouter(
           path="limit/:limit"
           element={<ListPage />}
           errorElement={<ErrorPage />}
-          loader={listLoader}
         >
           <Route path=":details" element={<CardLayout />}>
-            <Route
-              index
-              element={<CardPage />}
-              errorElement={<ErrorPage />}
-              loader={cardLoader}
-            />
+            <Route index element={<CardPage />} errorElement={<ErrorPage />} />
           </Route>
         </Route>
       </Route>
