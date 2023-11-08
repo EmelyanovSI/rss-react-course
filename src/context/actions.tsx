29a0a89b4a38b7e9e18ace515d5e84ef8f40120f
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-11-08 21:25:53 +0300

    refactor: update action interfaces to match new action types

--- src/context/actions.tsx
+++ src/context/actions.tsx
@@ -1,29 +1,55 @@
+import { Status } from '@/constants';
 import { Animal } from '@/interfaces/animal';
 import { getSearchFromStorage } from '@/utils';
 
 export enum ACTION_TYPE {
-  SEARCH = 'SEARCH',
-  LIST = 'LIST',
+  UPDATE_SEARCH = 'UPDATE_SEARCH',
+  GET_LIST_STARTED = 'GET_LIST_STARTED',
+  GET_LIST_SUCCESS = 'GET_LIST_SUCCESS',
+  GET_LIST_FAILURE = 'GET_LIST_FAILURE',
+  RESET_LIST = 'RESET_LIST',
 }
 
 export interface AppContextValue {
   search: string;
   list: Animal[];
+  status: Status;
+  message: string;
 }
 
 export const defaultValue: AppContextValue = {
   search: getSearchFromStorage(),
   list: [],
+  status: Status.Idle,
+  message: '',
 };
 
-interface SearchAction {
-  type: ACTION_TYPE.SEARCH;
+interface UpdateSearchAction {
+  type: ACTION_TYPE.UPDATE_SEARCH;
   payload: string;
 }
 
-interface ListAction {
-  type: ACTION_TYPE.LIST;
+interface GetListStartAction {
+  type: ACTION_TYPE.GET_LIST_STARTED;
+}
+
+interface GetListSuccessAction {
+  type: ACTION_TYPE.GET_LIST_SUCCESS;
   payload: Animal[];
 }
 
-export type AppContextAction = SearchAction | ListAction;
+interface GetListErrorAction {
+  type: ACTION_TYPE.GET_LIST_FAILURE;
+  payload: string;
+}
+
+interface ListResetAction {
+  type: ACTION_TYPE.RESET_LIST;
+}
+
+export type AppContextAction =
+  | UpdateSearchAction
+  | GetListStartAction
+  | GetListSuccessAction
+  | GetListErrorAction
+  | ListResetAction;
--- src/context/reducer.tsx
+++ src/context/reducer.tsx
@@ -1,17 +1,44 @@
+import { Status } from '@/constants';
 import { ACTION_TYPE, AppContextAction, AppContextValue } from './actions';
 
-export function reducer(value: AppContextValue, action: AppContextAction) {
+export function reducer(state: AppContextValue, action: AppContextAction) {
   switch (action.type) {
-    case ACTION_TYPE.SEARCH: {
+    case ACTION_TYPE.UPDATE_SEARCH: {
       return {
-        ...value,
+        ...state,
         search: action.payload,
       };
     }
-    case ACTION_TYPE.LIST: {
+    case ACTION_TYPE.GET_LIST_STARTED: {
       return {
-        ...value,
+        ...state,
+        list: [],
+        status: Status.Loading,
+        message: '',
+      };
+    }
+    case ACTION_TYPE.GET_LIST_SUCCESS: {
+      return {
+        ...state,
         list: action.payload,
+        status: Status.Succeeded,
+        message: '',
+      };
+    }
+    case ACTION_TYPE.GET_LIST_FAILURE: {
+      return {
+        ...state,
+        list: [],
+        status: Status.Failed,
+        message: action.payload,
+      };
+    }
+    case ACTION_TYPE.RESET_LIST: {
+      return {
+        ...state,
+        list: [],
+        status: Status.Idle,
+        message: '',
       };
     }
     default: {
