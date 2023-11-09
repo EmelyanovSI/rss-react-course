201d2f0484bcae66752ee2e14d5e939dbebc0c60
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-11-09 12:20:49 +0300

    feat(useSearch.ts): add custom hook for managing search functionality

---
+++ src/hooks/useSearch.ts
@@ -0,0 +1,22 @@
+import { ACTION_TYPE } from '@/context/actions';
+import { useAppContext, useAppReducer } from '@/context/hooks';
+import { setSearchFromStorage } from '@/utils';
+import { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+const useSearch = () => {
+  const { search } = useAppContext();
+  const dispatch = useAppReducer();
+  const [searchParams] = useSearchParams();
+
+  const searchParam = searchParams.get('search') ?? search;
+
+  useEffect(() => {
+    setSearchFromStorage(searchParam);
+    dispatch({ type: ACTION_TYPE.UPDATE_SEARCH, payload: searchParam });
+  }, [dispatch, searchParam]);
+
+  return search;
+};
+
+export default useSearch;
