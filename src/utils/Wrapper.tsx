d9f1580cbe54cc2e7918e1ff61126499249898b9
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-11-17 01:15:41 +0300

    refactor(Wrapper.tsx): wrap children component with ReduxProvider and PersistProvider

--- src/utils/Wrapper.tsx
+++ src/utils/Wrapper.tsx
@@ -1,12 +1,16 @@
-import AppProvider from '@/context';
+import { persistor, store } from '@/redux';
 import { FC, ReactElement } from 'react';
+import { Provider as ReduxProvider } from 'react-redux';
+import { PersistGate as PersistProvider } from 'redux-persist/integration/react';
 
 interface WrapperProps {
   children: ReactElement;
 }
 
 const Wrapper: FC<WrapperProps> = ({ children }) => (
-  <AppProvider>{children}</AppProvider>
+  <ReduxProvider store={store}>
+    <PersistProvider persistor={persistor}>{children}</PersistProvider>
+  </ReduxProvider>
 );
 
 export default Wrapper;
