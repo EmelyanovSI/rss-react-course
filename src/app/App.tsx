719b824280e767bfea1d7ce9df6aab5b9f034ca5
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-11-17 01:06:07 +0300

    feat(App.tsx): add redux store and persistor providers to enable state management and persistence in the app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,19 @@
 import Progress from '@/components/common/Progress';
 import ErrorBoundary from '@/components/ErrorBoundary';
-import AppProvider from '@/context';
+import { persistor, store } from '@/redux';
 import router from '@/router';
+import { Provider as ReduxProvider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
+import { PersistGate as PersistProvider } from 'redux-persist/integration/react';
 
 const App = () => {
   return (
     <ErrorBoundary>
-      <AppProvider>
-        <RouterProvider router={router} fallbackElement={<Progress />} />
-      </AppProvider>
+      <ReduxProvider store={store}>
+        <PersistProvider persistor={persistor} loading={<Progress />}>
+          <RouterProvider router={router} fallbackElement={<Progress />} />
+        </PersistProvider>
+      </ReduxProvider>
     </ErrorBoundary>
   );
 };
