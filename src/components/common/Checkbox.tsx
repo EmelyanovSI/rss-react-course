5aad352a3c55b9453fd6cc9432c7b3ca35ace76d
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-11-17 02:03:48 +0300

    feat(Checkbox.tsx): add Checkbox component
    The Checkbox component is a reusable component that allows users to select or deselect an option. It takes in the following props:
    - `label`: a string representing the label of the checkbox
    - `checked`: a boolean indicating whether the checkbox is checked or not
    - `onChange`: a callback function that is called when the checkbox is toggled
    
    The component renders a label element with a checkbox input and a label text. The appearance of the checkbox changes based on its checked state. When the checkbox is clicked, the `onChange` callback is called with the updated checked state.

---
+++ src/components/common/Checkbox.tsx
@@ -0,0 +1,40 @@
+import classNames from 'classnames';
+import { FC } from 'react';
+
+interface CheckboxProps {
+  label: string;
+  checked: boolean;
+  onChange: (checked: boolean) => void;
+}
+
+const Checkbox: FC<CheckboxProps> = ({ label, checked, onChange }) => {
+  const handleChange = () => {
+    onChange(!checked);
+  };
+
+  return (
+    <label
+      className={classNames(
+        'flex items-center gap-1 border-2',
+        'rounded-full whitespace-nowrap',
+        'hover:shadow active:shadow-none',
+        'transition duration-500 ease-in-out',
+        'border-red-100 h-10 px-6 select-none cursor-pointer',
+        { 'bg-red-100': checked }
+      )}
+    >
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={handleChange}
+        className={classNames(
+          'material-symbols-outlined',
+          '-ml-2 text-base cursor-pointer'
+        )}
+      />
+      <span className="cursor-pointer">{label}</span>
+    </label>
+  );
+};
+
+export default Checkbox;
