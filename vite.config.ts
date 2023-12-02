85ff77c6e653b22c2eeb08786f3eec2c0b248a9d
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-12-03 01:38:56 +0300

    chore: add .editorconfig, path mapping, alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
   esbuild: {
     jsxInject: `import React from 'react'`,
   },
