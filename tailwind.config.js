8441b50c905317d79779626d64859820e0997d01
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-12-03 01:42:42 +0300

    chore(tailwind.config.js): update tailwind configuration to extend theme with custom animation styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
-/** @type {import("tailwindcss").Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      animation: {
+        progress: 'progress 2s infinite',
+      },
+      keyframes: {
+        progress: {
+          '0%': { width: '0' },
+          '50%': { width: '50%' },
+          '100%': { width: '100%' },
+        },
+      },
+    },
+  },
   plugins: [],
 };
